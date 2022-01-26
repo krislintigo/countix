ac1440b09cffc75f4a4335a7ea7aee09fa5a30e0
1sses <vladzholon@gmail.com>
2022-01-26 09:20:01 +0300

    Working

---
+++ src/components/SalaryInputs.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <span class="big-span">
+      $
+      <input
+        type="number"
+        placeholder="Salary"
+        min="0"
+        :value="salary"
+        @input="$emit('updateSalary', +$event.target.value)"
+      >
+    </span>
+    <span class="big-span">
+      %
+      <input
+        type="number"
+        placeholder="Taxes"
+        min="0"
+        max="100"
+        :value="taxes"
+        @input="$emit('updateTaxes', +$event.target.value)"
+      >
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SalaryInputs",
+  props: {
+    salary: {
+      type: Number,
+      required: true
+    },
+    taxes: {
+      type: Number,
+      required: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
