bfef0c555bcb9231e0b9ddfece21a405ba750a7a
1sses <vladzholon@gmail.com>
2022-01-26 09:19:51 +0300

    Working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApexCharts from "vue-apexcharts"
+
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
+
 
 Vue.config.productionTip = false
 
