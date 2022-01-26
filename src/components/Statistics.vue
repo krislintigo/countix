e89cb523afb644c8b82766ffeda34e91cb6fc5ac
1sses <vladzholon@gmail.com>
2022-01-26 09:20:09 +0300

    Working

---
+++ src/components/Statistics.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <p class="stat-item">Net salary: <strong>{{netSalary}}$</strong></p>
+    <p class="stat-item">Planned expenses: <strong>{{plannedExpenses}}$</strong></p>
+    <p class="stat-item">Free money: <strong>{{freeMoney}}$</strong></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Statistics",
+  props: {
+    netSalary: {
+      type: Number,
+      required: true
+    },
+    plannedExpenses: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    freeMoney() {
+      return +(this.netSalary - this.plannedExpenses).toFixed(1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin-top: 40px;
+}
+
+.stat-item {
+  font-size: 24px;
+}
+</style>
