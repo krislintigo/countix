025873962c32a6cdc6218dbeb7a83ecd7b5eb9da
1sses <vladzholon@gmail.com>
2022-01-29 20:17:12 +0300

    Draggable items

--- src/components/ExpensesList.vue
+++ src/components/ExpensesList.vue
@@ -1,43 +1,44 @@
 <template>
-  <draggable v-model="basicExpenses">
-    <v-card
-      style="cursor: move;"
-      v-for="(expense, index) in basicExpenses"
-      :key="expense.id"
-      class="mb-3"
-    >
-      <v-card-title>{{ expense.name }}</v-card-title>
-      <v-card-subtitle>{{ expense.amount }}$</v-card-subtitle>
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            class="top-right mt-1"
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item>
-            <v-btn plain @click="$emit('edit', expense)">
-              <v-icon>mdi-pen</v-icon>
-              Edit
+  <draggable v-model="basicExpenses" v-bind="dragOptions" @start="drag = true" @end="drag = false">
+    <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+      <v-card
+        v-for="(expense, index) in basicExpenses"
+        :key="expense.id"
+        class="mb-3 list-group-item"
+      >
+        <v-card-title>{{ expense.name }}</v-card-title>
+        <v-card-subtitle>{{ expense.amount }}$</v-card-subtitle>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              class="top-right mt-1"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
             </v-btn>
-          </v-list-item>
-          <v-list-item>
-            <v-btn plain @click="$emit('delete', index)">
-              <v-icon>mdi-delete</v-icon>
-              Delete
-            </v-btn>
-          </v-list-item>
-          <v-list-item>
-            <v-checkbox v-model="expense.considered" @change="$emit('switch', index)" label="Switch"></v-checkbox>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-card>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-btn plain @click="$emit('edit', expense)">
+                <v-icon>mdi-pen</v-icon>
+                Edit
+              </v-btn>
+            </v-list-item>
+            <v-list-item>
+              <v-btn plain @click="$emit('delete', index)">
+                <v-icon>mdi-delete</v-icon>
+                Delete
+              </v-btn>
+            </v-list-item>
+            <v-list-item>
+              <v-checkbox v-model="expense.considered" @change="$emit('switch', index)" label="Switch"></v-checkbox>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-card>
+    </transition-group>
   </draggable>
 </template>
 
@@ -49,6 +50,11 @@ export default {
   components: {
     draggable
   },
+  data() {
+    return {
+      drag: false
+    }
+  },
   computed: {
     basicExpenses: {
       get() {
@@ -58,6 +64,16 @@ export default {
         this.$store.commit("setBasicExpenses", value);
       }
     },
+    dragOptions() {
+      return {
+        animation: 200,
+        group: 'description',
+        disabled: false,
+        ghostClass: 'my-ghost',
+        chosenClass: 'my-chosen',
+        dragClass: 'my-drag',
+      }
+    }
   }
 }
 </script>
@@ -67,5 +83,21 @@ export default {
   position: absolute;
   top: 0;
   right: 0;
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.my-ghost {
+  opacity: 1;
+}
+.my-chosen {
+  background: deepskyblue;
+}
+.list-group-item {
+  cursor: move;
 }
 </style>
